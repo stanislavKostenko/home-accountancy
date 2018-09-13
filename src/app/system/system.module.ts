311dfcc0412598f6fe530b9e6363dd5e606bd60d
Stanislav Kostenko <stas.kostenko.payever@gmail.com>
2018-09-13 18:01:37 +0300

    added side-nav

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { SystemRoutingModule } from './system-routing.module';
+import { SystemComponent } from './system.component';
+import { BillPageComponent } from './bill-page/bill-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { PlanningPageComponent } from './planning-page/planning-page.component';
+import { RecordsPageComponent } from './records-page/records-page.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MatButtonModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    SystemRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ],
-  declarations: []
+  declarations: [
+    SystemComponent,
+    BillPageComponent,
+    HistoryPageComponent,
+    PlanningPageComponent,
+    RecordsPageComponent,
+    SideNavComponent
+  ]
 })
-export class SystemModule { }
+export class SystemModule {
+}
